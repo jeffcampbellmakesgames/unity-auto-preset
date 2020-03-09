35195ad1467fc491490b962a96716d49cb1051a7
Jeff Campbell <jeff@zapdot.com>
2020-03-09 11:09:26 +0100

    v1.0.2
    * Added about window with version and developer information
    * Added menu items with helpful links to report bugs, support

---
+++ Scripts/Editor/AutoPresetMenuItems.cs
@@ -0,0 +1,31 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace JCMG.AutoPresets.Editor
+{
+	internal static class AutoPresetMenuItems
+	{
+		[MenuItem("Tools/JCMG/AutoPreset/Submit bug or feature request")]
+		internal static void OpenURLToGitHubIssuesSection()
+		{
+			const string GITHUB_ISSUES_URL = "https://github.com/jeffcampbellmakesgames/unity-auto-preset/issues";
+
+			Application.OpenURL(GITHUB_ISSUES_URL);
+		}
+
+		[MenuItem("Tools/JCMG/AutoPreset/Donate to support development")]
+		internal static void OpenURLToKoFi()
+		{
+			const string KOFI_URL = "https://ko-fi.com/stampyturtle";
+
+			Application.OpenURL(KOFI_URL);
+		}
+
+		[MenuItem("Tools/JCMG/AutoPreset/About")]
+		internal static void OpenAboutModalDialog()
+		{
+			AboutWindow.View();
+		}
+
+	}
+}
---
+++ Scripts/Editor/VersionConstants.cs
@@ -0,0 +1,11 @@
+﻿namespace JCMG.AutoPresets.Editor
+{
+	/// <summary>
+	/// Version info for this library.
+	/// </summary>
+	internal static class VersionConstants
+	{
+		// Version
+		public const string VERSION = "1.0.2";
+	}
+}
